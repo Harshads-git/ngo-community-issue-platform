ef4ed75a7ecb15234dfe0ed3e7fb74bf952f370f
Harshad S <sharshadcr7@gmail.com>
2026-03-28 17:23:24 +0530

    chore: update API_BASE_URL to live Render URL

--- frontend/website/js/config.js
+++ frontend/website/js/config.js
@@ -1,5 +1,5 @@
 // Configuration Variables for the Frontend
 // Change this URL when deploying the backend to a cloud service (e.g. Render, Railway, Heroku)
 const CONFIG = {
-    API_BASE_URL: 'http://localhost:5000'
+    API_BASE_URL: 'https://ngo-community-issue-platform.onrender.com'
 };
